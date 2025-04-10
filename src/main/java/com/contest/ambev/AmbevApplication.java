11225d9d5221fa357c232dc245006a659d2d77ba
Diego Gritti <diego.gritti.3@globalhitss.com.br>
2025-04-10 10:48:47 -0300

    Initial commit

---
+++ src/main/java/com/contest/ambev/AmbevApplication.java
@@ -0,0 +1,13 @@
+package com.contest.ambev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbevApplication.class, args);
+	}
+
+}
